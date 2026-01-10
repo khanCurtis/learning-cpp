0dccf69ffaaf4ce1c4c7fd8dd5a250f97772047f
Khanon Curtis <63666204+khanCurtis@users.noreply.github.com>
2026-01-09 22:50:01 -0700

    Removed ignored files from tracking

---
+++ 1.9introToLiteralsAndOperators/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 5 << '\n';  // prints the value of a literal
+
+    int x { 5 };
+    cout << x << '\n';  // prints the value of a variable
+
+    return 0;
+}
